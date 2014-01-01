8357c16f7a3ab7c0b88890b04068acd0098b66fd
kozaki <kozaki@asx.co.jp>
2014-01-01 17:42:11 +0900

    fixed bug.

--- KeyDown.php
+++ KeyDown.php
@@ -22,15 +22,22 @@ class WebDriverDemo extends Sauce\Sausage\WebDriverTestCase
         array(
             'browserName' => 'internet explorer',
             'desiredCapabilities' => array(
-                'version' => '10',
-                'platform' => 'Windows 8',
+                'version' => '9',
+                'platform' => 'Windows 7',
             )
         ),
         array(
             'browserName' => 'internet explorer',
             'desiredCapabilities' => array(
-                'version' => '9',
+                'version' => '8',
                 'platform' => 'Windows 7',
+            )
+        ),
+        array(
+            'browserName' => 'internet explorer',
+            'desiredCapabilities' => array(
+                'version' => '7',
+                'platform' => 'Windows XP',
             )
         ),
         array(
@@ -75,8 +82,16 @@ class WebDriverDemo extends Sauce\Sausage\WebDriverTestCase
         $this->url('https://s3-ap-northeast-1.amazonaws.com/logn.in/keydown.html');
     }
 
+    /**
+     * Send key and logging event attribute.
+     */
     public function testKeydown()
     {
+        $text = '';
+        $prev = '';
+
+        // U+0000 - U+00FF 255
+        // U+0000 - U+FFFF 65535
         for ($i = 0; $i <= 255; $i++)
         {
             $this->byId('input')->click();
@@ -85,6 +100,7 @@ class WebDriverDemo extends Sauce\Sausage\WebDriverTestCase
             $char = json_decode('"'.$unicodeChar.'"');
             $this->keys($char);
 
+            // fixed ie
             try
             {
                 $this->dismissAlert();
@@ -93,12 +109,26 @@ class WebDriverDemo extends Sauce\Sausage\WebDriverTestCase
             {
             }
 
-            foreach (split("<br/>", $this->byId('target')->text()) as $index => $line)
+			$this->byId('target')->click();
+
+            // fixed ie and other browser
+            if ($this->getBrowser() == 'internet explorer') {
+                $text = $this->byId('target')->text(); // get last send key
+            } else {
+                $text = $this->byId('target')->text(); // get all send key
+                $s = str_replace($prev, '', $text);
+                $prev = $text;
+                $text = $s;
+            }
+
+            // write log
+            foreach (split("<br/>", $text) as $index => $line)
             {
                 if ($index != 0 && $line == "")
                 {
                     continue;
                 }
+
                 $line = '"'.$unicodeChar.'", "'.addcslashes($char, "\0..\37").'", '.$line."\n";
                 file_put_contents('logs/keydown_'.$this->getBrowser().'_'.$this->getDesiredCapabilities()['version'].'_'.$this->getDesiredCapabilities()['platform'], $line, FILE_APPEND);
             }

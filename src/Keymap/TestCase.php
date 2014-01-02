fc7b09d60f4c50fcd4515363c97d2ef6bc758015
k-kinzal <dev+github@logn.in>
2014-01-02 19:49:17 +0900

    fixed show progress.

--- src/Keymap/TestCase.php
+++ src/Keymap/TestCase.php
@@ -6,50 +6,38 @@ class TestCase extends \Sauce\Sausage\WebDriverTestCase
     /**
      * Send key and logging event attribute.
      */
-    public function execute($name, $length)
+    public function execute($name, $char)
     {
         // initialize.
         $input = $this->byId('input');
         $target = $this->byId('target');
-        $str = ''; $prev = '';
-        // send keys U+0000 - length.
-        // U+0000 - U+00FF 255
-        // U+0000 - U+FFFF 65535
-        for ($i = 0; $i < $length; $i++) {
-            // clear textarea element.
-            $target->clear();
-            // active textbox element.
-            $input->click();
-            // send key.
-            $unicodeChar = sprintf('\u%04x', $i);
-            $char = json_decode('"'.$unicodeChar.'"');
-            $this->keys($char);
-            // fixed alert off on ie.
-            try { $this->dismissAlert(); } catch (\Exception $e) { }
-            // get result by javascript event.
-            // fixed ie and other browser.
-            if ($this->getBrowser() == 'internet explorer') {
-                $str = $target->text(); // get last send key
-            } else {
-                $str = $target->text(); // get all send key
-                $s = str_replace($prev, '', $str);
-                $prev = $str;
-                $str = $s;
-            }
-            $obj = json_decode('['.trim($str, ',').']', true); // $str format is {...},{...},
-            // logging
-            foreach ($obj as $index => $line) {
-                $line['name'] = $name;
-                $line['browser'] = $this->getBrowser();
-                $line['version'] = $this->getDesiredCapabilities()['version'];
-                $line['platform'] = $this->getDesiredCapabilities()['platform'];
-                $line['no'] = $index;
-                $line['unicode'] = $unicodeChar;
-                $line['glyph'] = addcslashes($char, "\0..\32");
+        // clear textarea element.
+        $target->clear();
+        // active textbox element.
+        $input->click();
+        // send key.
+        $unicodeChar = sprintf('\u%04x', $char);
+        $char = json_decode('"'.$unicodeChar.'"');
+        $this->keys($char);
+        // fixed alert off on ie.
+        try { $this->dismissAlert(); } catch (\Exception $e) { }
+        // get result by javascript event.
+        // fixed ie and other browser.
+        $str = $target->text();
+        $obj = json_decode('['.trim($str, ',').']', true); // $str format is {...},{...},
+        // logging
+        foreach ($obj as $index => $line) {
+            $line['name'] = $name;
+            $line['browser'] = $this->getBrowser();
+            $line['version'] = $this->getDesiredCapabilities()['version'];
+            $line['platform'] = $this->getDesiredCapabilities()['platform'];
+            $line['no'] = $index;
+            $line['unicode'] = $unicodeChar;
+            $line['glyph'] = addcslashes($char, "\0..\32");
 
-                \DB::insert($line);
-            }
+            \DB::insert($line);
         }
+        $this->assertTrue(true);
     }
 
 
